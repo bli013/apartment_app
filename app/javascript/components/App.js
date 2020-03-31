8d38e90825eac0ce8196a4534f3958a37910a28f
bli013 <btli@ymail.com>
2020-03-31 16:38:59 -0700

    Linked in the home page to react component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,12 +1,22 @@
 import React from "react"
 import { Nav, NavItem, NavLink } from 'reactstrap'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import MyListing from './pages/MyListing'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 class App extends React.Component {
   render () {
     return (
-      <>
-        <h1>This is the react app component</h1>
-        
-      </>
+      <Router>
+        <>
+          <h1>This is the react app component</h1>
+          <NavItem>
+            <NavLink to="/home" tag={ Link }>Home</NavLink>
+          </NavItem>
+        <Route path="/home" exact component={ Home } />  
+        </>
+      </Router>
     );
   }
 }
---
+++ app/javascript/components/pages/SignIn.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class SignIn extends React.Component {
+  render() {
+    return(
+      <h1>This is the SignIn Page</h1>
+    )
+  }
+}
+
+export default SignIn
