f0780d3c7831e4495947edd15c88d70b470914cc
bli013 <btli@ymail.com>
2020-03-31 15:20:32 -0700

    Initial commit, worked up to right before creating pages

---
+++ app/javascript/components/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Nav, NavItem, NavLink } from 'reactstrap'
+class App extends React.Component {
+  render () {
+    return (
+      <>
+        <h1>This is the react app component</h1>
+        
+      </>
+    );
+  }
+}
+
+export default App
